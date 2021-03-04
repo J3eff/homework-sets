2fcb1a4b6ab08ce0f743a6d5d65cbc30b27f1464
J3eff <jeffersonvideo125@gmail.com>
2021-03-04 18:33:06 -0300

    Exercicio Proposto - Trabalhando com Conjuntos

---
+++ ExercicioPropostio/Entities/OnlineCoursePortal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioPropostio.Entities
+{
+    class OnlineCoursePortal
+    {
+        public int UniqueCode { get; set; }
+
+        public OnlineCoursePortal(int uniqueCode)
+        {
+            UniqueCode = uniqueCode;
+        }
+
+        public override int GetHashCode()
+        {
+            return UniqueCode.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is OnlineCoursePortal))
+            {
+                return false;
+            }
+            OnlineCoursePortal other = obj as OnlineCoursePortal;
+            return UniqueCode.Equals(other.UniqueCode);
+        }
+
+
+    }
+}
---
+++ ExercicioPropostio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ExercicioPropostio.Entities;
+
+namespace ExercicioPropostio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<OnlineCoursePortal> set = new HashSet<OnlineCoursePortal>();
+
+            Console.Write("How many students for course A? ");
+            int A = int.Parse(Console.ReadLine());
+            for (int i = 0; i < A; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new OnlineCoursePortal(x));
+            }
+            Console.Write("How many students for course B? ");
+            int B = int.Parse(Console.ReadLine());
+            for (int i = 0; i < B; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new OnlineCoursePortal(x));
+            }
+            Console.Write("How many students for course C? ");
+            int C = int.Parse(Console.ReadLine());
+            for (int i = 0; i < C; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new OnlineCoursePortal(x));
+            }
+
+            Console.WriteLine("Total students: " + set.Count);
+
+        }
+    }
+}
